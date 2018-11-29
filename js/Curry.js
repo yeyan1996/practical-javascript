a50145cf7f3d5310924d49b94a112d74241d95b1
yeyan1996 <1366792281@qq.com>
2018-11-29 20:56:53 +0800

    change structure

--- js/Curry.js
+++ js/Curry.js
@@ -20,20 +20,18 @@ function add(num1, num2) {
 let curriedAdd = Curry1(add, 2, 3)
 
 /**
- * @description 函数柯里化2
+ * @description 函数柯里化2（根据柯里化后的函数执行多少次判断函数的结果）
  * @function Curry2
  * @param {*} fn -柯里化的函数
  * @returns {function} -柯里化后的函数
  */
 function Curry2(fn) {
     let args = []
-    return function next() {
-        //    debugger
+    return function next() { 
         if (arguments.length) {
             args.push(...arguments)
             return next
         } else {
-            console.log(...args);
             return fn(...args)
         }
     }
@@ -50,16 +48,24 @@ console.log(curriedAdd2(1)(2)(3)());
 
 
 /**
- * @description 函数柯里化3
+ * @description 函数柯里化3（根据柯里化前的函数决定柯里化后的函数需要执行多少次）
  * @function Curry2
  * @param {*} fn -柯里化的函数
  */
 function Curry3(fn) {
     if (fn.length <= 1) return fn;
-    const generator = (args, rest) => (rest ? arg => generator([...args, arg], rest - 1) : fn(...args));
+    const generator = function (args, rest) {
+        if (rest) {  //如果执行的次数未达到柯里化前的函数的参数个数
+            return function (arg) {
+               return generator([...args, arg], rest - 1)
+            }
+        } else {
+          return  fn(...args)
+    };
+}
 
 
-    return generator([], fn.length);
+return generator([], fn.length);
 };
 
 const add3 = (a, b, c) => a + b + c;
