ed20912603eb4a305383a8924c8690b5b7c81fd3
yeyan1996 <1366792281@qq.com>
2018-12-07 20:44:58 +0800

    docs:add debounce docs

--- js/Debounce.js
+++ js/Debounce.js
@@ -9,7 +9,7 @@
 
 export default  function Debounce(func,context,time) {
      return function () {
-         func.timeId ?  clearTimeout(func.timeId) : ''
+         func.timeId ?  clearTimeout(func.timeId) : '' //timeId必须是在函数外面能够读取的到的属性
          func.timeId = setTimeout(()=>{
              func.apply(context)
          },time)
