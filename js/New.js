c73e439817f33ee5acaccfc6ee1f3fdb8516ea1f
yeyan1996 <1366792281@qq.com>
2018-12-19 14:31:36 +0800

    '

--- js/New.js
+++ js/New.js
@@ -1,7 +1,10 @@
+const isComplexDataType = obj => {
+     return (typeof obj === 'object' || typeof obj === 'function') && obj !== null
+}
 const selfNew = function (fn, ...rest) {
     let instance = Object.create(fn.prototype)
     let res = fn.apply(instance, rest)
-    return typeof res === 'object' && res !== null ? res : instance
+    return isComplexDataType(res) ? res : instance
 }
 
 function Person(name, sex) {
