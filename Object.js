14f7b0b004e4ec5083e4f9e544496bee269921f7
yeyan1996 <1366792281@qq.com>
2019-01-07 13:20:39 +0800

    fix:selfAssign添加了对于source参数是字符串的处理

--- Object.js
+++ Object.js
@@ -5,9 +5,8 @@ const selfAssign = function (target, ...source) {
     if (target === null || target === undefined) throw new TypeError('不能传入null/undefined')
     isComplexDataType(target) || (target = new Object(target)) //变成一个基本包装类型
     return source.reduce((acc, cur) => {
-        // debugger
-        if (!isComplexDataType(acc)) return;
         // 遍历出Symbol属性和可枚举属性
+        // 字符串因为有iterator接口可被枚举,Object.assign会会遍历字符串所以可枚举的属性(没有length)
         [...Object.keys(cur), ...Object.getOwnPropertySymbols(cur)].forEach(key => {
             acc[key] = cur[key]
         })
@@ -31,15 +30,17 @@ let target = {
 let obj1 = {
     a: 2,
     b: 2,
+    c:undefined
 }
 
 let obj2 = {
     a: 3,
     b: 3,
-    [Symbol("a")]: 3
+    [Symbol("a")]: 3,
+    d:null
 }
 
-console.log(Object.selfAssign(target, obj1, obj2))
+console.log(Object.selfAssign(target, false, obj2))
 
 
 
