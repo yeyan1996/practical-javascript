dd819ba86de400d5e30967c446bc480c99f88afe
yeyan1996 <1366792281@qq.com>
2019-01-07 14:59:38 +0800

    update:添加了一些边界情况的处理(null,undefined),将基本类型参数转为基本包装类型

--- Object.js
+++ Object.js
@@ -1,12 +1,14 @@
+"use strict" //启用严格模式在尝试给基本包装类型已定义的下标赋值的时候报错
+
 const isComplexDataType = obj => (typeof obj === 'object' || typeof obj === 'function') && obj !== null
 
 //简单实现ES6的Object.assign
 const selfAssign = function (target, ...source) {
-    if (target === null || target === undefined) throw new TypeError('不能传入null/undefined')
-    isComplexDataType(target) || (target = new Object(target)) //变成一个基本包装类型
+    if (target == null) throw new TypeError('不能传入null/undefined')
     return source.reduce((acc, cur) => {
+        isComplexDataType(acc) || (acc = new Object(acc)); //变成一个基本包装类型
+        if (cur == null) return acc; //source为null,undefined时忽略
         // 遍历出Symbol属性和可枚举属性
-        // 字符串因为有iterator接口可被枚举,Object.assign会会遍历字符串所以可枚举的属性(没有length)
         [...Object.keys(cur), ...Object.getOwnPropertySymbols(cur)].forEach(key => {
             acc[key] = cur[key]
         })
@@ -30,17 +32,19 @@ let target = {
 let obj1 = {
     a: 2,
     b: 2,
-    c:undefined
+    c: undefined
 }
 
 let obj2 = {
     a: 3,
     b: 3,
     [Symbol("a")]: 3,
-    d:null
+    d: null
 }
 
-console.log(Object.selfAssign(target, false, obj2))
+console.log(Object.selfAssign(target,obj1,obj2))
+console.log(Object.selfAssign("abd", null, undefined))
+
 
 
 
