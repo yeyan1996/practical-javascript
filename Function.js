723d23ecae4d4b5e767fbe7c100668b56a6759d7
yeyan1996 <1366792281@qq.com>
2019-03-12 10:18:41 +0800

    update:bind

--- Function.js
+++ Function.js
@@ -1,21 +1,33 @@
-// 实现一个简易的bind的polyfill
+const isComplexDataType = obj => (typeof obj === 'object' || typeof obj === 'function') && obj !== null
+
+// 实现一个简易的bind
 const selfBind = function (bindTarget, ...args1) {
     if (typeof this !== 'function') throw new TypeError('this is not function')
     let func = this
     let boundFunc = function (...args2) {
         let args = [...args1, ...args2]
+
         // 使用new关键字调用返回新对象
-        return new.target ? func.apply(this, args) : func.apply(bindTarget, args)
+        if (new.target) {
+            let res = func.apply(this, args)
+            //如果构造函数返回一个对象则返回这个对象
+            if (isComplexDataType(res)) return res
+            //否则返回新建的对象
+            return this
+        } else {
+            func.apply(bindTarget, args)
+        }
     }
     // 绑定后的函数继承绑定前的函数(非箭头函数)
+    /**真正的bind创建的函数是没有prototype的,但是使用new会将创建的对象连接到bind前函数的prototype(非箭头函数)**/
     this.prototype && (boundFunc.prototype = Object.create(this.prototype))
 
     // 定义绑定后函数的长度和名字
-    let descrs = Object.getOwnPropertyDescriptors(func)
+    let desc = Object.getOwnPropertyDescriptors(func)
     Object.defineProperties(boundFunc, {
-        length: descrs.length,
-        name: Object.assign(descrs.name, {
-            value: `bound${descrs.name.value}`
+        length: desc.length,
+        name: Object.assign(desc.name, {
+            value: `bound${desc.name.value}`
         })
     })
     return boundFunc
@@ -32,6 +44,7 @@ Function.prototype.selfBind || (Object.defineProperty(Function.prototype, 'selfB
 
 function func() {
     this.name = 'yeyan1996'
+    return {}
 }
 
 let example = {
