04fb247297b83e175962e574611e97385d023c14
周浩磊 <1996yeyan@gmail.com>
2019-08-12 17:57:07 +0800

    doc:map.js 添加 reduce 实现 map 无法处理稀疏数组的注释

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 // 循环实现map
 const selfMap = function (fn, context) {
     let arr = Array.prototype.slice.call(this)
-    let mappedArr = Array(arr.length -1)
+    let mappedArr = Array()
     for (let i = 0; i < arr.length; i++) {
         // 判断稀疏数组的情况
         if (!arr.hasOwnProperty(i)) continue;
@@ -12,11 +12,10 @@ const selfMap = function (fn, context) {
 
 
 // reduce实现map
+// 由于 reduce 会跳过空单元数组，所以这个 polyfill 无法处理空单元数组
 const selfMap2 = function (fn, context) {
     let arr = Array.prototype.slice.call(this)
-    return arr.reduce((pre, cur, index) => {
-        return [...pre, fn.call(context, cur, index, this)]
-    }, [])
+    return arr.reduce((pre, cur, index) => [...pre, fn.call(context, cur, index, this)], [])
 }
 
 
@@ -33,6 +32,6 @@ Array.prototype.selfMap2 || (Object.defineProperty(Array.prototype, 'selfMap2', 
     writable: true
 }))
 
-let arr = ['z', 'h', 'l']
+let arr = ['z', 'h', ,'l']
 console.log(arr.selfMap(item => item + "1"))
 console.log(selfMap2.call({0:'a',1:'b',length:2}, item => item + "1")) // map 方法同样支持类数组
